bc46a401a636fb1db62e1aee926061feca64247a
ameer017 <rajiabdullahi907@gmail.com>
2025-03-09 22:19:47 +0100

    restructuring the folder tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import Navbar from "./components/Navbar";
-import { icons, images } from "./constants";
-import { About, Footer, Hero, Services } from "./sections";
+
 
 function App() {
   return (
     <main className="relative overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Footer />
+      
     </main>
   );
 }
--- src/sections/index.js
+++ src/sections/index.js
@@ -3,9 +3,4 @@ import About from "./About";
 import Footer from "./Footer";
 import Services from "./Services";
 
-export {
-    About,
-    Footer,
-    Hero,
-    Services
-};
+export { About, Footer, Hero, Services };
