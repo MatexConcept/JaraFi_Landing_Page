0299cb88af141dbd9d64dc52447d6a3e9d329538
ameer017 <rajiabdullahi907@gmail.com>
2025-03-09 22:24:59 +0100

    restructuring completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-
+import { Header, HeroSection, About, Features, FAQ, Footer } from "./index.js";
 
 function App() {
   return (
     <main className="relative overflow-x-hidden">
-      
+      <Header />
+      <HeroSection />
+      <About />
+      <Features />
+      <FAQ />
+      <Footer />
     </main>
   );
 }
