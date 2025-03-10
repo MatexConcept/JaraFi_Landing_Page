be303342e7eb9a1c010e71386a9b9fd53dfa3889
ameer017 <rajiabdullahi907@gmail.com>
2025-03-10 21:46:23 +0100

    Added the join waitlist functionality to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,45 +2,139 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { RiMenu2Fill, RiCloseFill } from "react-icons/ri";
 import { motion, AnimatePresence } from "framer-motion";
+import Modal from "./Modal";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitError, setSubmitError] = useState(null);
+  const [success, setSuccess] = useState("");
+  const [existingEmails, setExistingEmails] = useState([]);
+  const sheetdbapi = import.meta.env.VITE_SHEET_DB;
+
+  // console.log(import.meta.env.VITE_SHEET_DB);
+
+  const handleCloseModal = () => {
+    window.document.body.classList.remove("overflow-hidden");
+
+    setShowModal(false);
+    setSubmitError(null);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setSubmitError(null);
+
+    const formData = new FormData(e.target);
+    const name = formData.get("fullName");
+    const email = formData.get("email");
+
+    try {
+      const firstResponse = await axios.post(
+        "https://jarafibackend.vercel.app/waitlist/join",
+        { fullname: name, email },
+        { withCredentials: true }
+      );
+
+      // console.log({ firstResponse });
+
+      if (firstResponse.status === 201) {
+        const response = await fetch(
+          `https://sheetdb.io/api/v1/${sheetdbapi}`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              data: [
+                {
+                  name: name,
+                  email: email,
+                },
+              ],
+            }),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        handleCloseModal();
+        setSuccess("You have been added to wait list successfully");
+        toast.success(
+          "Thanks for joining our wait list we'll keep you updated"
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error(`${error.response.data.message}`);
+      setSubmitError("An error occurred. Please try again.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
-    <header className="flex justify-between items-center p-6 bg-[#F2EDE4] w-full fixed top-0 left-0 z-50 border-b-[1px] border-[#0F0140]">
-      <Link to="/">
-        <img src="/JaraFi-blue.png" className="w-[88px]" alt="JaraFi Logo" />
-      </Link>
+    <>
+      <header className="flex justify-between items-center p-6 bg-[#F2EDE4] w-full fixed top-0 left-0 z-50 border-b-[1px] border-[#0F0140]">
+        <Link to="/">
+          <img src="/JaraFi-blue.png" className="w-[88px]" alt="JaraFi Logo" />
+        </Link>
 
-      <div className="hidden md:flex">
-        <button className="p-[10px] border border-primary rounded-[10px] bg-[#0F0140] text-[#E7E6E7] text-[16px]">
-          Join Our Waitlist
+        <div className="hidden md:flex">
+          <button
+            className="p-[10px] border border-primary rounded-[10px] bg-[#0F0140] text-[#E7E6E7] text-[16px]"
+            onClick={() => setShowModal(true)}
+          >
+            Join Our Waitlist
+          </button>
+        </div>
+
+        <button
+          className="md:hidden text-[#0F0140] focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <RiCloseFill size={28} /> : <RiMenu2Fill size={28} />}
         </button>
-      </div>
 
-      <button
-        className="md:hidden text-[#0F0140] focus:outline-none"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        {isOpen ? <RiCloseFill size={28} /> : <RiMenu2Fill size={28} />}
-      </button>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="absolute top-full left-0 w-full bg-[#F2EDE4] flex flex-col items-center space-y-4 p-6 shadow-lg md:hidden h-screen"
+            >
+              <button
+                className="p-[10px] border border-primary rounded-lg bg-[#0F0140] text-[#E7E6E7] text-[16px]"
+                onClick={() => setShowModal(true)}
+              >
+                Join Our Waitlist
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </header>
 
       <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="absolute top-full left-0 w-full bg-[#F2EDE4] flex flex-col items-center space-y-4 p-6 shadow-lg md:hidden h-screen"
-          >
-            <button className="p-[10px] border border-primary rounded-lg bg-[#0F0140] text-[#E7E6E7] text-[16px]">
-              Join Our Waitlist
-            </button>
-          </motion.div>
+        {showModal && (
+          <Modal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+            onSubmit={handleSubmit}
+            isSubmitting={isSubmitting}
+            submitError={submitError}
+          />
         )}
       </AnimatePresence>
-    </header>
+    </>
   );
 };
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <section className="bg-[#F2EDE4] min-h-[660px] mt-[60px] flex justify-center items-center">
+      <div className="w-full max-w-2xl text-center flex flex-col items-center gap-6">
+        <h1 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] font-bold font-[Merriweather Sans] leading-tight relative flex flex-wrap justify-center items-center gap-2 text-center">
+          One app for all your{" "}
+          <span className="relative inline-block bg-[#F2E205] px-2  rounded-full shadow-md overflow-hidden w-[180px] sm:w-[220px] md:w-[280px]">
+            <motion.div
+              className="flex space-x-4"
+              animate={{ x: ["100%", "-100%"] }}
+              transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
+            >
+              <span className="uppercase">stablecoins</span>
+              {/* <span className="uppercase">stablecoins</span> */}
+            </motion.div>
+          </span>{" "}
+          needs.
+        </h1>
 
-export default HeroSection
+        <p className="text-[18px] sm:text-[20px] font-[Montserrat] px-4">
+          Send crypto to anyone, anywhere with just their phone number.
+        </p>
+
+        <button className="flex gap-[6px] items-center text-[16px] bg-[#0F0140] px-5 py-3 rounded-lg text-[#E7E6E7] font-[Montserrat] my-10 cursor-pointer shadow-md hover:bg-[#0A002F] transition-all">
+          Get on Playstore
+          <img src="/icons8-playstore-48.png" className="w-[20px]" />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
