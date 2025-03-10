045c37293fd03e7ce0fa0b011432fdb0bbaa225e
ameer017 <rajiabdullahi907@gmail.com>
2025-03-10 15:04:03 +0100

    Layout --> FAQ done ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import Layout from "./components/Layout.jsx";
 import { Header, HeroSection, About, Features, FAQ, Footer } from "./index.js";
 
 function App() {
   return (
     <main className="relative overflow-x-hidden">
-      <Header />
-      <HeroSection />
-      <About />
-      <Features />
-      <FAQ />
-      <Footer />
+      <Layout>
+        <HeroSection />
+        <About />
+        <Features />
+        <FAQ />
+      </Layout>
     </main>
   );
 }
